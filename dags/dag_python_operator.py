5312937408ea8814375c11137ef20bba6d530719
hjchae <rino980715@gmail.com>
2023-10-18 10:41:17 +0900

    python_dag upload

---
+++ dags/dag_python_operator.py
@@ -0,0 +1,26 @@
+import datetime
+
+import pendulum
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+
+with DAG(
+    dag_id="dags_python_operator",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2023, 10, 12, tz="Asia/Seoul"),
+    catchup=False,
+    dagrun_timeout=datetime.timedelta(minutes=60),
+) as dag:
+    def select_fruit():
+        fruit = ['APPLE','BANANA','ORANGE','AVOKADO']
+        rand_it = random.randit(0,3)
+        print(fruit[rand_int])
+
+    py_t1 = PythonOperator(
+        task_id='py_t1',
+        python_callable=select_fruit
+    )
+
+    py_t1
